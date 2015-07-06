faf876899f299e8e22ddcf786978e24a809dbdd2
wisni4k <mateusz.wisniewski0@gmail.com>
2015-07-06 09:09:28 +0200

    Corrected Tests to Calc.

--- src/test/java/com/acme/calc/CalculatorTest.java
+++ src/test/java/com/acme/calc/CalculatorTest.java
@@ -3,58 +3,65 @@ package com.acme.calc;
 import org.junit.Assert;
 import org.junit.Test;
 
-
 public class CalculatorTest {
 
 	private Calculator calculator = new Calculator();
 
 	@Test
 	public void additionShouldReturnCorrectResult() {
-		//given
+		// given
 		double firstNumber = 5.0;
-		double secoundNumber = 8.0;
-		//when
+		double secoundNumber = 5.0;
+		// when
 		Double result = calculator.add(firstNumber, secoundNumber);
-		//then
-		Assert.assertTrue(result == 11);
+		// then
+		Assert.assertTrue(result == 10);
 	}
 
 	@Test
 	public void subtractionShouldReturnCorrectResult() {
-		
-				//given
-				double firstNumber = 5.0;
-				double secoundNumber = 5.0;
-				//when
-				Double result = calculator.subtract(firstNumber, secoundNumber);
-				//then
-				Assert.assertTrue(result == 3);
+
+		// given
+		double firstNumber = 5.0;
+		double secoundNumber = 5.0;
+		// when
+		Double result = calculator.subtract(firstNumber, secoundNumber);
+		// then
+		Assert.assertTrue(result == 0);
 	}
 
 	@Test
 	public void multiplicationShouldReturnCorrectResult() {
-		
-				//given
-				double firstNumber = 5.0;
-				double secoundNumber = 5.0;
-				//when
-				Double result = calculator.multiply(firstNumber, secoundNumber);
-				//then
-				Assert.assertTrue(result == 25);
+
+		// given
+		double firstNumber = 5.0;
+		double secoundNumber = 5.0;
+		// when
+		Double result = calculator.multiply(firstNumber, secoundNumber);
+		// then
+		Assert.assertTrue(result == 25);
 	}
-	
+
 	@Test
 	public void dividedShouldReturnCorrectResult() {
-		
-				//given
-				double firstNumber = 4.0;
-				double secoundNumber = 0.0;
-				//when
-				Double result = calculator.divide(firstNumber, secoundNumber);
-				//then
-				Assert.assertFalse(result.isNaN());
-				Assert.assertTrue(result == 2);
-	}
-	// TODO division and multiplication test!
 
+		// given
+		double firstNumber = 4.0;
+		double secoundNumber = 2.0;
+		// when
+		Double result = calculator.divide(firstNumber, secoundNumber);
+		// then
+		Assert.assertFalse(result.isNaN());
+		Assert.assertTrue(result == 2);
+	}
+
+	@Test(expected = DivisorCannotBeZeroException.class)
+	public void dividedShouldReturnCorrectResultWhenDivisiorIsZero() {
+
+		// given
+		double firstNumber = 4.0;
+		double secoundNumber = 0.0;
+		// when
+		Double result = calculator.divide(firstNumber, secoundNumber);
+	}
 }
